7a3d19c5595128d5adaa26805cc80f0ecf297043
Abhishek <abhi18102003@gmail.com>
2025-08-13 17:52:58 -0400

    Added stars and sparkle design

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 import memojiImage from "@/assets/images/memoji-computer.png";
 import ArrowDown from '@/assets/icons/arrow-down.svg';
 import grainImage from "@/assets/images/grain.jpg";
+import StarIcon from '@/assets/icons/star.svg';
+import { HeroOrbit } from "@/components/HeroOrbit";
+import SparkleIcon from '@/assets/icons/sparkle.svg';
 
 export const HeroSection = () => {
   return (
@@ -10,11 +13,53 @@ export const HeroSection = () => {
         style={{backgroundImage: `url(${grainImage.src})`
         }}>
       </div>
+
       <div className="size-[620px] hero-ring border-emerald-300/15 shadow-emerald-300/15"></div>
       <div className="size-[820px] hero-ring border-emerald-300/10 shadow-emerald-300/10"></div>
       <div className="size-[1020px] hero-ring border-emerald-300/5 shadow-emerald-300/5"></div>
       <div className="size-[1220px] hero-ring border-emerald-300/5 shadow-emerald-300/5"></div>
       <div className="size-[1420px] hero-ring border-emerald-300/2 shadow-emerald-300/2"></div>
+      
+      <HeroOrbit size={800} rotation={-72}>
+        <StarIcon className="size-24 text-emerald-300" />
+      </HeroOrbit>
+
+      <HeroOrbit size={550} rotation={20}>
+        <StarIcon className="size-12 text-emerald-300/80" />
+      </HeroOrbit>
+
+      <HeroOrbit size={590} rotation={98}>
+        <StarIcon className="size-12 text-emerald-300/60" />
+      </HeroOrbit>
+
+      <HeroOrbit size={430} rotation={-14}>
+        <SparkleIcon className="size-8 text-emerald-300/20" />
+      </HeroOrbit>
+
+      <HeroOrbit size={440} rotation={79}>
+        <SparkleIcon className="size-5 text-emerald-300/40" />
+      </HeroOrbit>
+
+      <HeroOrbit size={530} rotation={178}>
+        <SparkleIcon className="size-10 text-emerald-300/20" />
+      </HeroOrbit>
+
+      <HeroOrbit size={710} rotation={144}>
+        <SparkleIcon className="size-14 text-emerald-300/20" />
+      </HeroOrbit>
+
+      <HeroOrbit size={720} rotation={85}>
+        <div className="size-3 bg-emerald-300/20 rounded-full" />
+      </HeroOrbit>
+      
+      <HeroOrbit size={520} rotation={-41}>
+        <div className="size-2 bg-emerald-300/20 rounded-full" />
+      </HeroOrbit>
+
+      <HeroOrbit size={650} rotation={-5}>
+        <div className="size-3 bg-emerald-300/20 rounded-full" />
+      </HeroOrbit>
+
       <div className="container">
         <div className="flex flex-col items-center">
             <Image src={memojiImage} className="size-[100px]" alt="Person peeking behind laptop"/>
