4495709e5226276cc546dfa7128bb6b80125eb1d
Abhishek <abhi18102003@gmail.com>
2025-08-14 22:29:15 -0400

    Created Reusable components Cards and Section Header

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,3 +1,15 @@
+import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
+
 export const AboutSection = () => {
-  return <div>About Section</div>;
+  return (
+    <div>
+      <SectionHeader 
+        eyebrow="About Me" 
+        title="A Glimpse Into My World" 
+        description="Learn more about who i am, what i do, and what inspires me." 
+      />
+
+    </div>
+  );
 };
